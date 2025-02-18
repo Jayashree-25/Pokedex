4635d2270071e5d548916b1bb18fe0df27c977c7
jayashre-25 <dasjayashree8260@gmail.com>
2025-02-18 23:46:31 +0530

    fetching data

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ let currentPage = 1;
 
 const fetchPokemon = async () => {
     const pokemonCount = 151;  //first 151 pokemon 
-// we can fetch all the pokemon but pokeAPI have rate limit to prevent excessive load on the server. Fetching data for all Pokémon (which is well over 1000) could hit the rate limit, especially if you're making many requests in a short time.
+    // we can fetch all the pokemon but pokeAPI have rate limit to prevent excessive load on the server. Fetching data for all Pokémon (which is well over 1000) could hit the rate limit, especially if you're making many requests in a short time.
     const promises = [];  //array to hold all the fetch requests for Pokémon
-} 
+
+    for (let i = 1; i <= pokemonCount; i++) {
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            .then(res => res.json()))
+    }
+
+    allPokemon = await Promise.all(promises);    //waits for all the fetch requests to complete and then stores the result in allPokemon
+    updatePagination(); 
+}
+
+// const displayPokemon = (pokemonList) => {
+//     pokedox.innerHTML = ""; //clears any existing Pokémon displayed before adding new ones
+//     pokemonList.forEach(pokemon => {
+//         const pokemonElement = document.createElement("div");
+//         pokemonElement.classList.add("pokemon");
+//     })
+// }
